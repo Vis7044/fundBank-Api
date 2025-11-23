feaa1539f7630ce9ecae027be3d6e21acccbcec2
affan <affanaslam24@gmail.com>
2025-11-23 23:32:36 +0530

    Database connection

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
-package server
+package main
 
 import (
-	"fmt"
+	"github.com/funcBank_Api/config"
+	// 	"github.com/funcBank_Api/controllers"
+	// 	"github.com/funcBank_Api/repositories"
+	// 	"github.com/funcBank_Api/routes"
+	// 	"github.com/funcBank_Api/services"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
-func StartServer() {
-	fmt.Println("Server started")
+
+func main() {
+	//Load environment variables
+	config.LoadConfig()
+
+	// Connect to database
+	config.ConnectDb()
+	defer config.DisconnectDatabase()
+
+	// Initialize router
+	r := gin.Default()
+
+	// Allow CORS for Angular frontend
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 60 * 60,
+	}))
+
+	// Run server
+	r.Run(":8080")
 }
