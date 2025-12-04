ce51be3a2f8d6e0ffc3d8e0b532ee9a2a4aa3b81
Sahil Sajar <sahilsajar@Sahils-MacBook-Air.local>
2025-12-05 00:51:13 +0530

    Category routes api

--- utils/response.go
+++ utils/response.go
@@ -11,6 +11,8 @@ type Response[T any] struct {
 	Data    T    `json:"data"`
 }
 
+// It fetches a query parameter from the URL, converts it into an int64,
+// and returns a default value if the parameter is missing.
 func GetQueryInt64(ctx gin.Context, key string, defaultValue int64) (int64, error) {
 	valueStr := ctx.Query(key)
 	if valueStr == "" {
